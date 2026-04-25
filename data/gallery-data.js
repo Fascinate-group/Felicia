eb788e7e35da10c1d2b52aab8e8dc942cfd51b36
dfbeclipse <dfbeclipse@users.noreply.github.com>
2026-04-26 00:16:42 +0900

    ギャラリー更新：17.494.png追加・HFoazkTakAIBEZa.jpg削除

--- data/gallery-data.js
+++ data/gallery-data.js
@@ -6,6 +6,7 @@ window.GALLERY_DATA = [
   { image: 'images/gallery/07.230.png' },
   { image: 'images/gallery/08.182.png' },
   { image: 'images/gallery/08.902.png' },
+  { image: 'images/gallery/17.494.png' },
   { image: 'images/gallery/18.333.png' },
   { image: 'images/gallery/24.815.png' },
   { image: 'images/gallery/25.686.png' },
@@ -17,7 +18,6 @@ window.GALLERY_DATA = [
   { image: 'images/gallery/HCPfga2aAAA4vRL.jpg' },
   { image: 'images/gallery/HCeg63IbAAAbMy0.jpg' },
   { image: 'images/gallery/HFoBx4bakAEPblD.jpg' },
-  { image: 'images/gallery/HFoazkTakAIBEZa.jpg' },
   { image: 'images/gallery/HFodDvlakAIF_nl.jpg' },
   { image: 'images/gallery/HGaVI1obYAANVQ4.jpg' },
 ];
