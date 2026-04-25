f57c5a4caf4bbd8c018bbcd7d038b35d314dfa27
dfbeclipse <dfbeclipse@users.noreply.github.com>
2026-04-25 23:59:36 +0900

    ギャラリー画像13枚追加

--- data/gallery-data.js
+++ data/gallery-data.js
@@ -1,6 +1,19 @@
 // ギャラリー画像は images/gallery/ フォルダに配置してください
 // 新しい画像を追加する際はこのリストにファイル名を追記してください
 window.GALLERY_DATA = [
+  { image: 'images/gallery/00.562.png' },
+  { image: 'images/gallery/05.841.png' },
+  { image: 'images/gallery/07.230.png' },
+  { image: 'images/gallery/08.182.png' },
+  { image: 'images/gallery/08.902.png' },
+  { image: 'images/gallery/18.333.png' },
+  { image: 'images/gallery/24.815.png' },
+  { image: 'images/gallery/25.686.png' },
+  { image: 'images/gallery/26.302.png' },
+  { image: 'images/gallery/28.023.png' },
+  { image: 'images/gallery/36.302.png' },
+  { image: 'images/gallery/53.452.png' },
+  { image: 'images/gallery/58.235.png' },
   { image: 'images/gallery/HCPfga2aAAA4vRL.jpg' },
   { image: 'images/gallery/HCeg63IbAAAbMy0.jpg' },
   { image: 'images/gallery/HFoBx4bakAEPblD.jpg' },
